7849742f26423ffe0fb830bc0e95f043019bcea8
VueMid <turabovumid587@gmail.com>
2025-04-28 16:00:12 +0500

    Fix: Header section is fixed (04.28.2025/16:00)

--- src/components/ui/DrawerHeader.jsx
+++ src/components/ui/DrawerHeader.jsx
@@ -1,8 +1,21 @@
+import React from "react"
 export default function DrawerHeader() {
     return (
         <div>
-            <header>
-                
+            <header className="drawerHeader">
+                <div className="container drawerHeader__container">
+                    <div className="drawerHeader__top-box">
+                        <Link href={'/'}>
+                            <img className="drawerHeader__left-close-burger inline-block" src="/icons/close__burger.svg" alt="open__burger" />
+                        </Link>
+                        <Link href={'/'}>
+                            <img className="drawerHeader__middle-mini-logo inline-block w-[67px] h-[37px]" src="/icons/mini__logo.svg" alt="mini__logo" />
+                        </Link>
+                        <Link href={'/'}>
+                            <img className="drawerHeader__left-open-burger inline-block" src="/icons/open__burger.svg" alt="open__burger" />
+                        </Link>
+                    </div>
+                </div>
             </header>
         </div>
     )
