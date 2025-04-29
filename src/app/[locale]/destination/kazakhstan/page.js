2dff27ee19db366c3830133773314d9608e3f0cc
sunnatbekweb <sunnatbekkadamov@gmail.com>
2025-04-29 15:41:02 +0500

    created destination

---
+++ src/app/[locale]/destination/kazakhstan/page.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Kazahstan() {
+  return <div>page</div>;
+}
---
+++ src/app/[locale]/destination/tajikistan/page.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Tajikistan() {
+  return <div>page</div>;
+}
