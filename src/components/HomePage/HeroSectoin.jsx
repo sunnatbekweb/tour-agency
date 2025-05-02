e81618fe73ed87b8a5742faf24a3a690c4659946
VueMid <turabovumid587@gmail.com>
2025-05-02 17:41:04 +0500

    Continue...(05.02.2025/17:42)

--- src/components/HomePage/HeroSectoin.jsx
+++ src/components/HomePage/HeroSectoin.jsx
@@ -1,9 +1,51 @@
 import './HeroSectoin.css'
 import SecondContactButton from '../ui/SecondContactButton'
+import { Link } from '@/i18n/navigation'
 export default function HeroSectoin() {
+    const link = [
+        {
+            icon: 'icons/right__arrow.svg',
+            path: '/uzbekistan',
+            label: 'Uzbekistan',
+        },
+        {
+            icon: 'icons/right__arrow.svg',
+            path: '/kazakhstan',
+            label: 'Kazakhstan',
+        },
+        {
+            icon: 'icons/right__arrow.svg',
+            path: '/tajikistan',
+            label: 'Tajikistan',
+        },
+        {
+            icon: 'icons/right__arrow.svg',
+            path: '/turkmanistan',
+            label: 'Turkmanistan',
+        },
+        {
+            icon: 'icons/right__arrow.svg',
+            path: '/kyrgizistain',
+            label: 'Kyrgizistain',
+        }
+    ]
     return (
         <div>
-            <section className='hero'>
+            <section className='hero relative'>
+                <nav className='hero__nav absolute bottom-0'>
+                    <ul className='hero__list'>
+                        {link.map((item, index) => (
+                            <li className='hero__lists w-full' key={index}>
+                                <hr className='w-full bg-white/20 h-[1px] border-none outline-none' />
+                                <Link className='hero__links' href={item.path}>
+                                    <p className='hero__link-texts font-medium text-[24px] uppercase text-white'>{item.label}</p>
+                                    <img className='hero__link-icons' src={item.icon} alt="icons" />
+                                </Link>
+                                <hr className='w-full bg-white/20 h-[1px] border-none outline-none' />
+                            </li>
+                        ))}
+                    </ul>
+                </nav>
                 <div className="container hero__container flex flex-col justify-center items-center min-h-screen gap-y-[89px] px-6 md:px-14 xl:pt-[290px]">
                     <div className="hero__top-box flex flex-col justify-center items-center">
                         <p className='hero__top-text text-center w-full font-medium text-[16px] tracking-tighter-[-2%] text-white mb-6 md:text-[32px] md:mb-[32px] lg:text-[40px] lg:mb-4'>
