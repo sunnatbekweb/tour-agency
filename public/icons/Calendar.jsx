fbd72f87323c0439cde41dbe59742439770552b7
sunnatbekweb <sunnatbekkadamov@gmail.com>
2025-05-08 20:54:33 +0500

    updated trip-finder and created pagination

---
+++ public/icons/Calendar.jsx
@@ -0,0 +1,119 @@
+import React from "react";
+
+export const Calendar = () => {
+  return (
+    <svg
+      width="32"
+      height="33"
+      viewBox="0 0 32 33"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clip-path="url(#clip0_479_1111)">
+        <mask
+          id="mask0_479_1111"
+          style={{ maskType: "luminance" }}
+          maskUnits="userSpaceOnUse"
+          x="0"
+          y="0"
+          width="32"
+          height="33"
+        >
+          <path d="M0 0.337984H31.9999V32.3379H0V0.337984Z" fill="white" />
+        </mask>
+        <g mask="url(#mask0_479_1111)">
+          <path
+            d="M28.2499 31.0879H3.74999C2.36925 31.0879 1.25 29.9686 1.25 28.5879V6.50465C1.25 5.12391 2.36925 4.00466 3.74999 4.00466H28.2499C29.6307 4.00466 30.7499 5.12391 30.7499 6.50465V28.5879C30.7499 29.9686 29.6307 31.0879 28.2499 31.0879Z"
+            stroke="#C3C3C3"
+            strokeWidth="2.5"
+            strokeMiterlimit="10"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+          <path
+            d="M30.7499 11.4219H1.25"
+            stroke="#C3C3C3"
+            strokeWidth="2.5"
+            strokeMiterlimit="10"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+          <path
+            d="M8.625 1.58723V6.50391"
+            stroke="#C3C3C3"
+            strokeWidth="2.5"
+            strokeMiterlimit="10"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+          <path
+            d="M23.375 1.58723V6.50391"
+            stroke="#C3C3C3"
+            strokeWidth="2.5"
+            strokeMiterlimit="10"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+          <path
+            d="M7.39453 18.1289H9.8529"
+            stroke="#C3C3C3"
+            strokeWidth="2.5"
+            strokeMiterlimit="10"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+          <path
+            d="M14.7695 18.1289H17.2279"
+            stroke="#C3C3C3"
+            strokeWidth="2.5"
+            strokeMiterlimit="10"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+          <path
+            d="M22.1445 18.1289H24.6029"
+            stroke="#C3C3C3"
+            strokeWidth="2.5"
+            strokeMiterlimit="10"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+          <path
+            d="M7.39453 24.3379H9.8529"
+            stroke="#C3C3C3"
+            strokeWidth="2.5"
+            strokeMiterlimit="10"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+          <path
+            d="M14.7695 24.3379H17.2279"
+            stroke="#C3C3C3"
+            strokeWidth="2.5"
+            strokeMiterlimit="10"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+          <path
+            d="M22.1445 24.3379H24.6029"
+            stroke="#C3C3C3"
+            strokeWidth="2.5"
+            strokeMiterlimit="10"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </g>
+      </g>
+      <defs>
+        <clipPath id="clip0_479_1111">
+          <rect
+            width="32"
+            height="32"
+            fill="white"
+            transform="translate(0 0.337891)"
+          />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
