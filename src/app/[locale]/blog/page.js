cf8ebbcbd7a12fd799c9fc464e22fb9121718478
sunnatbekweb <sunnatbekkadamov@gmail.com>
2025-05-13 17:11:25 +0500

    created blog page hero

--- src/app/[locale]/blog/page.js
+++ src/app/[locale]/blog/page.js
@@ -1,5 +1,16 @@
 import React from "react";
+import "@/styles/page_styles/blog.css";
 
 export default function Blog() {
-  return <div>Blog</div>;
+  return (
+    <>
+      <section className="blog_hero">
+        <div className="container h-full flex items-center justify-center">
+          <h1 className="w-full sm:w-1/2 lg:max-w-[860px] font-medium text-7xl lg:text-9xl text-white text-center">
+            Wander. Discover. Share.
+          </h1>
+        </div>
+      </section>
+    </>
+  );
 }
