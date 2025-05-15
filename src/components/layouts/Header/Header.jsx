3227ddebced40575d1ca8898921d2aaa57537a3b
VueMid <turabovumid587@gmail.com>
2025-05-15 21:33:54 +0500

    Continue...(05.15.2025/21:34)

--- src/components/layouts/Header/Header.jsx
+++ src/components/layouts/Header/Header.jsx
@@ -67,24 +67,37 @@ export const Header = () => {
       <header className={`header fixed top-0 w-full z-[1111] ${isScrolled ? "backdrop-blur-2xl shadow-2xl duration-500 bg-black/40" : "duration-500"}`}>
         <div className="container header__container w-full flex flex-row justify-between items-center px-6 py-12 md:px-9 md:py-[52px] xl:py-[36px]">
           <div className="header__left-box">
-            <button className="header__left-open-burger-button" onClick={() => setIsOpen(true)}>
+            <button
+              className="header__left-open-burger-button"
+              onClick={() => setIsOpen(true)}>
               <img className="header__left-open-burger-icon inline-block w-[40px] h-[40px] md:w-[72px] md:h-[72px]" src="/icons/open__burger.svg" alt="open__burger" />
             </button>
             <Link href={'/'}>
-              <img className="header__left-mini-logo inline-block xl:active:opacity-50 xl:duration-100 lg:w-[140px] lg:h-[77px]" src="/icons/logo.svg" alt="logo" />
+              <img
+                className="header__left-mini-logo inline-block xl:active:opacity-50 xl:duration-100 lg:w-[140px] lg:h-[77px]"
+                src="/icons/logo.svg"
+                alt="logo" />
             </Link>
           </div>
           <div className="header__middle-box">
             <Link href={'/'}>
-              <img className="header__middle-mini-logo inline-block w-[67px] h-[37px] md:w-[140px] md:h-[77px]" src="/icons/logo.svg" alt="logo" />
+              <img
+                className="header__middle-mini-logo inline-block w-[67px] h-[37px] md:w-[140px] md:h-[77px]"
+                src="/icons/logo.svg"
+                alt="logo" />
             </Link>
             <nav className='header__middle-nav'>
               <ul className='header__middle-list'>
                 {link.map((item, index) =>
                   item.label === "Destinations" ? (
-                    <li key={index} className='header__middle-lists relative group'>
+                    <li
+                      key={index}
+                      className='header__middle-lists relative group'>
                       <div className='header__middle-links bg-white/70 cursor-pointer flex items-center px-4 py-2'>
-                        <img className='header__middle-links-texts-icons' src={item.icon} alt="icon" />
+                        <img
+                          className='header__middle-links-texts-icons'
+                          src={item.icon}
+                          alt="icon" />
                         <p className='header__middle-links-texts font-bold uppercase text-[#656267] xl:16px 2xl:text-[20px]'>
                           {item.label}
                         </p>
@@ -106,8 +119,13 @@ export const Header = () => {
                     </li>
                   ) : (
                     <li key={index}>
-                      <Link href={item.path} className='header__middle-links bg-white/70 flex items-center px-4 py-2'>
-                        <img className='header__middle-links-texts-icons' src={item.icon} alt="icon" />
+                      <Link
+                        href={item.path}
+                        className='header__middle-links bg-white/70 flex items-center px-4 py-2'>
+                        <img
+                          className='header__middle-links-texts-icons'
+                          src={item.icon}
+                          alt="icon" />
                         <p className='header__middle-links-texts font-bold uppercase text-[#656267] xl:16px 2xl:text-[20px]'>{item.label}</p>
                       </Link>
                     </li>
@@ -117,10 +135,15 @@ export const Header = () => {
           </div>
           <div className='header__right-box flex flex-row justify-center items-center gap-x-[48px]'>
             <div className='header__right-contact-button hidden'>
-              <SecondContactButton>Contact us</SecondContactButton>
+              <SecondContactButton>
+                Contact us
+              </SecondContactButton>
             </div>
             <div className="header__right-language bg-white/10 rounded-[33px] border-[#A28887] border-[1px] flex flex-row justify-center items-center gap-x-[5px] py-[7.5px] px-[7.5px] md:gap-x-[9px] md:py-[14px] md:px-[14px] xl:py-[8px] 2xl:px-[18px] 2xl:py-[18px]">
-              <img className="header__right-language-icon object-cover w-[16px] h-[16px] md:w-[33px] md:h-[33px]" src="/icons/language__icon.svg" alt="language__icon" />
+              <img
+                className="header__right-language-icon object-cover w-[16px] h-[16px] md:w-[33px] md:h-[33px]"
+                src="/icons/language__icon.svg"
+                alt="language__icon" />
               <p className="header__right-language-text font-medium text-[11px] leading-[100%] text-white md:text-[16px] 2xl:text-[20px]">
                 Eng
               </p>
@@ -128,7 +151,9 @@ export const Header = () => {
           </div>
         </div>
       </header>
-      <DrawerHeader modal={isOpen} close={closeBurger} />
+      <DrawerHeader
+        modal={isOpen}
+        close={closeBurger} />
     </div>
   )
 }
--- src/components/ui/FooterSmSection.jsx
+++ src/components/ui/FooterSmSection.jsx
@@ -1,6 +1,9 @@
 "use client"
 import { Link } from '@/i18n/navigation'
 import './FooterSmSection.css'
+import { useEffect, useRef, useState } from 'react'
+import { Slide, ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 export default function FooterSmSection() {
     const moveTop = () => {
         window.scrollTo(0, 0)
@@ -36,10 +39,94 @@ export default function FooterSmSection() {
             links: 'https://www.instagram.com/umid_dev'
         },
     ]
+    const [form, setForm] = useState({ email: '' });
+    const [correct, setCorrect] = useState({ email: '' });
+    const [error, setError] = useState({ email: false });
+    const [isFormValid, setIsFormValid] = useState(false);
+    const emailRef = useRef()
+    if (form.email.length > 0) {
+        if (error.email) {
+            emailRef.current.style.borderColor = "red";
+            emailRef.current.style.borderWidth = "2px";
+        } else {
+            emailRef.current.style.borderColor = "green";
+            emailRef.current.style.borderWidth = "2px";
+        }
+    }
+    useEffect(() => {
+        const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        if (form.email.length === null) {
+            setError({ ...error, email: '*Please enter your email.' });
+            setCorrect({ ...correct, email: false });
+        } else if (!emailRegex.test(form.email)) {
+            setError({ ...error, email: '*Please enter a valid email address.' });
+            setCorrect({ ...correct, email: false });
+        } else {
+            setCorrect({ ...correct, email: 'This field has been filled correctly!' });
+            setError({ ...error, email: false });
+        }
+    }, [form.email])
+    useEffect(() => {
+        setIsFormValid(correct.email)
+    }, [correct])
+    async function sendData(email) {
+        const TOKEN = `7331186148:AAEXcMJUHFCgpzm1TghB9itA7WT0KHlVu3M`
+        const botID = '-1001837026407'
+        const info = `User  %0A<strong>📧: </strong>${email}`;
+        const response = await fetch(`https://api.telegram.org/bot${TOKEN}/sendMessage?chat_id=${botID}&text=${info}&parse_mode=html`);
+        const data = await response.json();
+        console.log(data);
+        setForm({ email: "" });
+    }
+    const formSubmission = async (e) => {
+        e.preventDefault();
+        if (isFormValid) {
+            await sendData(form.email);
+            toast.success("Successful!", {
+                position: "top-right",
+                autoClose: 2000,
+                hideProgressBar: false,
+                closeOnClick: false,
+                pauseOnHover: false,
+                draggable: true,
+                progress: undefined,
+                theme: "colored",
+                transition: Slide,
+            });
+
+        } else {
+            toast.error("Error", {
+                position: "top-right",
+                autoClose: 2000,
+                hideProgressBar: false,
+                closeOnClick: false,
+                pauseOnHover: false,
+                draggable: true,
+                progress: undefined,
+                theme: "colored",
+                transition: Slide,
+            });
+        }
+    }
     return (
-        <div>
-            <footer className='footerSmSection w-full bg-[#A98D7D] rounded-tr-[30px] rounded-tl-[30px] py-[52px]'>
-                <div className="container w-full footerSmSection__container flex flex-col justify-center items-center px-6 md:px-9">
+        <div className='md:hidden'>
+            <ToastContainer
+                position="top-right"
+                autoClose={2000}
+                hideProgressBar={false}
+                newestOnTop
+                closeOnClick={false}
+                rtl={false}
+                pauseOnFocusLoss={false}
+                draggable
+                pauseOnHover={false}
+                theme="colored"
+                transition={Slide}
+            />
+            <footer
+                className='footerSmSection w-full bg-[#A98D7D] rounded-tr-[30px] rounded-tl-[30px] py-[52px]'
+                onSubmit={formSubmission}>
+                <div className="container w-full footerSmSection__container flex flex-col justify-center items-center px-6">
                     <div className="footerSmSection__top w-full flex flex-row justify-between items-start ">
                         <Link href={'/'}>
                             <img
@@ -52,14 +139,21 @@ export default function FooterSmSection() {
                         </p>
                     </div>
                     <form className='footerSmSection__form w-full flex flex-row justify-center items-center mt-[41px]'>
-                        <div className='footerSmSection__form-box w-full flex flex-col justify-items-center items-start gap-2'>
-                            <div className='footerSmSection__form-input-box w-full flex flex-row justify-start items-center relative bg-white rounded-[26.5px] py-[15px]'>
+                        <div className='footerSmSection__form-box w-full flex flex-col justify-items-center items-start gap-2 h-[90px]'>
+                            <div
+                                className='footerSmSection__form-input-box w-full flex flex-row justify-start items-center relative bg-white rounded-[26.5px] py-[15px]'
+                                ref={emailRef}>
                                 <input
-                                    className="formSection__form-email-input outline-none border-none pl-[24px]"
+                                    className="footerSmSection__form-email-input w-[78%] outline-none pl-[24px] font-medium text-[18px] leading-[24px] text-[#323232]"
+                                    onChange={(e) => {
+                                        e.preventDefault();
+                                        setForm({ ...form, email: e.target.value.trimStart() })
+                                    }}
                                     type="email"
                                     id="email"
-                                    placeholder="Enter Your email"
-                                    autoComplete="on"
+                                    placeholder="Enter your email"
+                                    autoComplete="off"
+                                    value={form.email}
                                     required
                                 />
                                 <button className='footerSmSection__form-button flex justify-center items-center rounded-[20.8px] right-[24px] absolute top-[6px] bottom-[6px] bg-[#A98D7D] px-[18px] py-[9px] active:opacity-50'>
@@ -69,12 +163,11 @@ export default function FooterSmSection() {
                                         alt="right__submit-icon" />
                                 </button>
                             </div>
-                            <p className="footerSmSection__form-text text-[#CF734A]">
-
-                            </p>
-                            <p className="footerSmSection__form-text text-green-800">
-
-                            </p>
+                            {error.email && form.email.length > 0 ? (
+                                <p className="text-red-500">
+                                    {error.email}
+                                </p>
+                            ) : null}
                         </div>
                     </form>
                     <div className="footerSmSection__bottom-box w-full flex flex-row justify-between items-start mt-[56px]">
@@ -84,7 +177,9 @@ export default function FooterSmSection() {
                                     Explore
                                 </h4>
                                 {link.map((item, index) => (
-                                    <li className='footerSmSection__lists flex flex-col justify-center items-start' key={index}>
+                                    <li
+                                        className='footerSmSection__lists flex flex-col justify-center items-start'
+                                        key={index}>
                                         <Link className='footerSmSection__links flex flex-col justify-center items-start'>
                                             <p className='footerSmSection__links-text font-medium text-[16px] leading-[100%] tracking-tighter-[-2%] text-white/70 active:opacity-50'>
                                                 {item.links}
@@ -103,25 +198,44 @@ export default function FooterSmSection() {
                             </q>
                         </div>
                     </div>
-                    <div className='footerSmSection__icon-box flex flex-row justify-center items-center gap-[70px] mt-[123px]'>
+                    <div className='footerSmSection__icon-box flex flex-row justify-center items-center gap-[70px] mt-[58px]'>
                         {icon.map((icons, id) => (
-                            <div className="footerSmSection__icon-mini-boxes" key={id}>
-                                <a className='footerSmSection__icon-links' href={icons.links} target="_blank" rel="noopener noreferrer">
-                                    <img className='footerSmSection__icon-images object-cover inline-block w-[48px] h-[48px]' src={icons.icons} alt={icons.descriptions} />
+                            <div
+                                className="footerSmSection__icon-mini-boxes"
+                                key={id}>
+                                <a className='footerSmSection__icon-links'
+                                    href={icons.links}
+                                    target="_blank"
+                                    rel="noopener noreferrer">
+                                    <img className='footerSmSection__icon-images object-cover inline-block w-[48px] h-[48px]'
+                                        src={icons.icons}
+                                        alt={icons.descriptions} />
                                 </a>
                             </div>
                         ))}
                     </div>
-                    <button className='footerSmSection__button mt-[44px] active:duration-100 duration-100 active:opacity-50 border-4 border-white/50 rounded-full outline-none' onClick={moveTop}>
-                        <img className='footerSmSection__button-icon rotate-[316deg]' src={'/icons/white__arrow-icon.svg'} alt="button__icon" />
+                    <button
+                        className='footerSmSection__button mt-[44px] active:duration-100 duration-100 active:opacity-50 border-4 border-white/50 rounded-full outline-none'
+                        onClick={moveTop}>
+                        <img
+                            className='footerSmSection__button-icon rotate-[316deg]'
+                            src={'/icons/white__arrow-icon.svg'}
+                            alt="button__icon" />
                     </button>
-                    <div className="footerSmSection__copy-box flex flex-col justify-center items-center gap-4 mt-[40px] xl:gap-12">
+                    <hr className='border-none outline-none bg-white/15 w-full h-[2px] my-[30px]' />
+                    <div className="footerSmSection__copy-box flex flex-col justify-center items-center gap-4 xl:gap-12">
                         <p className="footerSmSection__copy-text font-medium text-[19px] leading-[24px] tracking-tighter-[-2%] text-white uppercase">
                             &copy; SRW 2025
                         </p>
                         <div className="footerSmSection__copy-logo-box flex flex-row justify-center items-center">
-                            <p className="footerSmSection__developing-text font-medium text-[19px] leading-[24px] tracking-tighter-[-2%] text-white">Developed by:&nbsp;&nbsp;</p>
-                            <a className="footerSmSection__copy-logo-link" href="https://www.onesystem.uz" target="_blank" rel="noopener noreferrer">
+                            <p className="footerSmSection__developing-text font-medium text-[19px] leading-[24px] tracking-tighter-[-2%] text-white">
+                                Developed by:&nbsp;&nbsp;
+                            </p>
+                            <a
+                                className="footerSmSection__copy-logo-link"
+                                href="https://www.onesystem.uz"
+                                target="_blank"
+                                rel="noopener noreferrer">
                                 <p className="footerSmSection__copy-logo-text font-medium text-[19px] leading-[24px] tracking-tighter-[-2%] text-white active:opacity-50">
                                     OneSystem
                                 </p>
