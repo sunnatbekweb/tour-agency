f6579a443789422c811c53b62a14c2adc46605c1
sunnatbekweb <sunnatbekkadamov@gmail.com>
2025-05-29 17:19:33 +0500

    added trip filter to destinations

--- src/components/ui/TripSearch.jsx
+++ src/components/ui/TripSearch.jsx
@@ -57,7 +57,7 @@ export default function TripSearch() {
     getTripTheme();
   }, []);
   return (
-    <div className="w-full px-2.5 lg:px-8 py-4 lg:py-10 rounded-2xl lg:rounded-4xl bg-[#CCB9AEE5] border border-[#989898] absolute -top-[42px] md:-top-[146px] xl:-top-[85px]">
+    <div className="w-full px-2.5 lg:px-8 py-4 lg:py-10 rounded-2xl lg:rounded-4xl bg-[#CCB9AEE5] border border-[#989898] absolute -top-[42px] md:-top-[103px] xl:-top-[85px]">
       <form
         onSubmit={handleSubmit}
         className="hidden p-2 rounded-2xl md:rounded-[46px] bg-transparent xl:bg-white xl:border border-[#E6E6E6] md:grid grid-cols-2 xl:grid-cols-4 gap-4 items-center font-medium"
@@ -88,7 +88,10 @@ export default function TripSearch() {
             ))}
           </select>
         </label>
-        <label htmlFor="duration" className="flex items-center gap-x-6 bg-white rounded-3xl px-6">
+        <label
+          htmlFor="duration"
+          className="flex items-center gap-x-6 bg-white rounded-3xl px-6"
+        >
           <div>
             <Clock />
           </div>
@@ -127,7 +130,7 @@ export default function TripSearch() {
             ))}
           </select>
         </label>
-        <button className="w-full h-[70px] col-span-2 xl:col-span-1 rounded-[40px] bg-[#A5958B] border-4 xl:border-none border-white font-medium text-2xl text-white">
+        <button className="w-full h-[70px] rounded-[40px] bg-[#A5958B] border-4 xl:border-none border-white font-medium text-2xl text-white">
           Search
         </button>
       </form>
@@ -140,7 +143,12 @@ export default function TripSearch() {
           className="w-full ps-3 focus:outline-none"
         />
         <button className="px-3 py-2 rounded-3xl bg-[#A5958B]">
-          <Image src={'/icons/search_icon.svg'} width={24} height={24} alt="Search" />
+          <Image
+            src={"/icons/search_icon.svg"}
+            width={24}
+            height={24}
+            alt="Search"
+          />
         </button>
       </form>
     </div>
