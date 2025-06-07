d34cfefb207502f06ed6f874949bd7bda5d89dd1
VueMid <turabovumid587@gmail.com>
2025-06-07 20:47:28 +0500

    Continue...(06.07.2025/20:48)

--- src/components/Providers.jsx
+++ src/components/Providers.jsx
@@ -1,9 +1,7 @@
 'use client';
-
 import { NextIntlClientProvider } from 'next-intl';
 import { Provider } from 'react-redux';
 import { store } from '@/lib/store';
-
 export default function Providers({ children, locale, messages }) {
   return (
     <NextIntlClientProvider locale={locale} messages={messages}>
--- src/components/ui/RoadSection.jsx
+++ src/components/ui/RoadSection.jsx
@@ -1,15 +1,15 @@
-import { Link } from '@/i18n/navigation';
-import './RoadSection.css';
-import RightChevron from '../../../public/icons/RightChevron';
+import { Link } from "@/i18n/navigation";
+import "./RoadSection.css";
+import RightChevron from "../../../public/icons/RightChevron";
 export default function RoadSection() {
   return (
     <div>
       <section className="road xl:mt-[282px] hidden xl:w-full xl:block">
         <div className="container road__container xl:flex xl:flex-col xl:justify-center xl:items-center xl:px-6">
-          <Link className="road-logo-link" href={'/'}>
+          <Link className="road-logo-link" href={"/"}>
             <img
               className="road__logo-image xl:inline-block xl:w-[140px] xl:h-[77px] xl:mb-[76px]"
-              src={'/icons/gold__logo.svg'}
+              src={"/icons/gold__logo.svg"}
               alt="logo"
             />
           </Link>
@@ -21,10 +21,12 @@ export default function RoadSection() {
               In Silk road Journey
             </h3>
           </div>
-          <button className="road__button xl:flex xl:flex-row xl:justify-center xl:items-center xl:gap-3.5 xl:active:opacity-50">
-            Let’s travel
-            <RightChevron />
-          </button>
+          <Link href={"/contact"}>
+            <button className="road__button xl:flex xl:flex-row xl:justify-center xl:items-center xl:gap-3.5 xl:active:opacity-50">
+              Let’s travel
+              <RightChevron />
+            </button>
+          </Link>
         </div>
       </section>
     </div>
