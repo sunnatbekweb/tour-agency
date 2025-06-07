9d815ff0e7fa581b00c25565f902193dd914ddfc
VueMid <turabovumid587@gmail.com>
2025-06-07 16:03:04 +0500

    Continue...(06.07.2025/16:04)

--- src/components/TurkmenistanPage/FifthFilterSection.jsx
+++ src/components/TurkmenistanPage/FifthFilterSection.jsx
@@ -51,7 +51,7 @@ export default function FifthFilterSection() {
             <button
               key={item.id}
               onClick={() => handleCountrySelect(item)}
-              className={`fifthFilter__top-buttons w-full transition-all duration-500 ease-in-out flex justify-center items-center px-[22px] py-[10px] rounded-[16px] font-medium text-[16px] leading-[100%] md:text-[32px] md:px-[25px] md:py-[17px] md:rounded-[32px]
+              className={`fifthFilter__top-buttons transition-all duration-500 ease-in-out flex justify-center items-center px-[22px] py-[10px] rounded-[16px] font-medium text-[16px] leading-[100%] md:text-[32px] md:px-[25px] md:py-[17px] md:rounded-[32px]
               ${
                 selectedCountry.id === item.id
                   ? "bg-[#B4A297] text-white"
