bd0950802a289e13907908e6e45ee7dda1425904
sunnatbekweb <sunnatbekkadamov@gmail.com>
2025-06-11 18:50:29 +0500

    updated about page localization

--- src/app/[locale]/about/page.js
+++ src/app/[locale]/about/page.js
@@ -4,13 +4,16 @@ import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import { AboutFAQ } from "@/components/about/AboutFAQ";
 import axios from "axios";
-import { useLocale } from "next-intl";
+import { useLocale, useTranslations } from "next-intl";
 import "@/styles/page_styles/about.css";
+import { useExpertise } from "@/store/data";
 
 export default function About() {
   const [tripLeaders, setTripLeaders] = useState();
   const [statistics, setStatistics] = useState();
+  const expertise = useExpertise();
   const locale = useLocale();
+  const t = useTranslations();
   const getTripLeaders = async () => {
     try {
       await axios
@@ -36,14 +39,18 @@ export default function About() {
   return (
     <>
       <section className="about relative">
-        <div className="container h-full flex flex-col items-center 2xl:items-start justify-center">
-          <h1 className="2xl:ml-[10%] w-full flex flex-col items-center 2xl:items-start justify-center font-medium text-3xl sm:text-5xl md:text-7xl xl:text-9xl text-white">
-            <span>Not Just Tours</span>
-            <span className=" ml-[20%] 2xl:ml-[30%]">We Tell Stories</span>
+        <div className="container h-full flex flex-col items-center justify-center">
+          <h1
+            className={`w-full flex flex-col items-center font-medium text-white ${locale === "ru" ? "text-xl sm:text-3xl md:text-5xl xl:text-7xl" : " text-2xl sm:text-3xl md:text-5xl xl:text-7xl"}`}
+          >
+            <span>{t("about.hero.text1")}</span>
+            <span className={`${locale === "ru" ? "" : "ml-[15%]"}`}>
+              {t("about.hero.text2")}
+            </span>
           </h1>
           <div className="absolute bottom-20">
-            <button className="w-[162px] h-[48px] lg:w-[216px] lg:h-[72px] bg-white font-medium text-xl lg:text-2xl text-[#656267] rounded-4xl 2xl:hidden">
-              Contact us
+            <button className="w-[162px] h-[48px] lg:w-[216px] lg:h-[72px] bg-white font-medium` text-xl `lg:text-2xl text-[#656267] rounded-4xl 2xl:hidden">
+              {t("header.contact")}
             </button>
           </div>
         </div>
@@ -51,19 +58,14 @@ export default function About() {
       <section className="py-20">
         <div className="container px-6">
           <span className="font-medium text-xl text-[#A5958B] uppercase block mb-8">
-            Trip Blogs
+            {t("header.nav.about")}
           </span>
-          <div className="flex flex-col gap-y-8 lg:flex-row gap-x-5 items-center font-medium text-[#323232] uppercase">
-            <h2 className="text-3xl md:text-5xl lg:text-7xl w-full lg:w-1/2">
-              Where Cultures Converge and History Lives
+          <div className="flex flex-col gap-y-8 lg:flex-row gap-x-5 font-medium text-[#323232] uppercase">
+            <h2 className="text-2xl md:text-4xl lg:text-6xl w-full lg:w-1/2">
+              {t("about.section1.title")}
             </h2>
             <p className="text-xs md:text-lg lg:text-2xl w-full lg:w-1/2">
-              Silk Road Wonders is more than just a travel company — it’s your
-              gateway to the legendary Silk Road. We craft immersive journeys
-              that connect the past with the present, guiding you through
-              ancient caravan routes, vibrant cultures, and the enduring spirit
-              of trade that once linked civilizations.Each journey with us is a
-              step into history, reimagined for the modern explorer.
+              {t("about.section1.text")}
             </p>
           </div>
         </div>
@@ -77,38 +79,21 @@ export default function About() {
       <section className="py-20">
         <div className="container px-6">
           <span className="font-medium text-xl text-[#A5958B] uppercase block mb-8">
-            Trip Blogs
+            {t("header.nav.about")}
           </span>
           <div className="flex flex-col gap-y-12 lg:flex-row gap-x-5 font-medium text-[#323232]">
             <h3 className="text-3xl md:text-5xl lg:text-7xl w-full lg:w-1/2 uppercase">
-              Explore Our Exclusive Tour
+              {t("about.section2.title")}
             </h3>
             <div className="w-full lg:w-1/2">
               <p className="text-xs md:text-lg lg:text-2xl uppercase">
-                Our crew is always getting bigger, but we all work toward one
-                goal: to make sales success not only possible but inevitable for
-                teams everywhere.
+                {t("about.section2.subtitle")}
               </p>
               <p className="text-xs md:text-lg lg:text-2xl mt-8 lg:mt-[100px] mb-8 lg:mb-[72px]">
-                In 2022, at vero eos et accusamus et iusto odio dignissimos
-                ducimus qui blanditiis praesentium voluptatum deleniti atque
-                corrupti quos dolores et quas molestias excepturi sint occaecati
-                cupiditate non provident, similique sunt in culpa qui officia
-                deserunt mollitia animi, id est laborum et dolorum fuga. Et
-                harum quidem rerum facilis est et expedita distinctio. Nam
-                libero tempore, cum soluta nobis est eligendi optio cumque nihil
-                impedit quo minus id quod maxime placeat facere possimus, omnis
-                voluptas assumenda est, omnis dolor repellendus. Temporibus
-                autem quibusdam et aut officiis debitis aut rerum necessitatibus
-                saepe eveniet ut et voluptates repudiandae sint et molestiae non
-                recusandae. Itaque earum rerum hic tenetur a sapiente delectus,
-                ut aut reiciendis voluptatibus maiores alias consequatur aut
-                perferendis doloribus asperiores repellat.
+                {t("about.section2.text")}
               </p>
               <h4 className="text-xl md:text-3xl lg:text-5xl uppercase">
-                “Our goal is to build software that gives customer-facing teams
-                at SMBs the ability to create fruitful and enduring
-                relationships with customers.”
+                {t("about.section2.bottom_text")}
               </h4>
             </div>
           </div>
@@ -157,10 +142,10 @@ export default function About() {
       <section className="bg-[#F0E6E0] pt-20 pb-[120px]">
         <div className="container px-6">
           <span className="font-medium text-xl text-[#A5958B] uppercase block mb-8">
-            Trip Blogs
+            {t("header.nav.about")}
           </span>
-          <h3 className="font-medium text-3xl md:text-5xl lg:text-7xl w-full lg:w-1/2 uppercase mb-[72px]">
-            Meet our amazing tour leaders
+          <h3 className="font-medium text-xl md:text-3xl lg:text-5xl w-full lg:w-1/2 uppercase mb-[72px]">
+            {t("about.tour_leaders")}
           </h3>
           <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-4 gap-y-10">
             {tripLeaders?.results?.map((leader) => (
@@ -197,31 +182,24 @@ export default function About() {
         <div className="container px-6">
           <div className="flex flex-col gap-y-8 lg:flex-row items-center mb-[60px]">
             <h3 className="font-medium text-3xl md:text-5xl lg:text-7xl w-full lg:w-1/2 uppercase">
-              our Expertise
+              {t("about.expertise.title")}
             </h3>
             <p className="font-medium md:text-2xl w-full lg:w-[30%] ml-auto">
-              At Silk Road Adventures, we pride ourselves on delivering
-              exceptional travel experiences. Here’s what sets us apart:
+              {t("about.expertise.subtitle")}
             </p>
           </div>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-            {[...Array(6)].map((_, index) => (
+            {expertise.map((_, index) => (
               <div
                 key={index}
                 className="flex flex-col items-center md:block px-6 md:px-12 py-8 bg-white border border-[#EBEBEB] rounded-2xl font-medium text-[#323232]"
               >
                 <div className="w-fit bg-[#B4A297] rounded-full p-4 mb-8">
-                  <Image
-                    src={"/icons/expertise_icon.svg"}
-                    width={40}
-                    height={400}
-                    alt="Icon"
-                  />
+                  <Image src={_.icon} width={40} height={400} alt="Icon" />
                 </div>
-                <h4 className="text-lg md:text-2xl mb-4">Cultural Knowledge</h4>
+                <h4 className="text-lg md:text-2xl mb-4">{_.title}</h4>
                 <p className="text-sm lg:text-lg text-center md:text-left">
-                  Deep understanding of Central Asia and Silk Road traditions,
-                  history, and local customs.
+                  {_.text}
                 </p>
               </div>
             ))}
@@ -232,10 +210,10 @@ export default function About() {
         <div className="container question__container w-full flex flex-col justify-center items-start gap-[40px] px-6 md:gap-[94px] md:px-9">
           <div className="question__top-box w-full lg:w-1/2 flex flex-col justify-center items-start gap-2 md:gap-4">
             <p className="question__text text-[#A5958B] font-medium text-xl uppercase">
-              Trip founder
+              {t("header.nav.about")}
             </p>
-            <h2 className="question__title font-medium text-3xl md:text-5xl lg:text-7xl text-[#323232] uppercase">
-              frequently asked question
+            <h2 className="question__title font-medium text-2xl md:text-4xl lg:text-6xl text-[#323232] uppercase">
+              {t("home_question.home_question-text")}
             </h2>
           </div>
           <AboutFAQ />
