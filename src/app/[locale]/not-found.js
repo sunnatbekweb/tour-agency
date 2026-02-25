055aa3ceed92727a93904ede26a1bf9905652d2d
sunnatbekweb <sunnatbekkadamov@gmail.com>
2026-02-25 11:49:47 +0500

    not-found.js error fix

---
+++ src/app/[locale]/not-found.js
@@ -0,0 +1,11 @@
+export default function NotFound() {
+	return (
+		<>
+			<section className="notFound">
+				<div className="container notFound__container">
+					<h2>salom</h2>
+				</div>
+			</section>
+		</>
+	)
+}
